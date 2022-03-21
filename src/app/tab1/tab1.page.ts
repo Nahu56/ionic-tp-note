0fcbe89d64d7b70e591dd2f6f664c5aaad3b3055
Nahu56 <60613115+Nahu56@users.noreply.github.com>
2022-03-21 15:34:12 +0100

    Master

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Toast } from '@capacitor/toast';
+import { Network } from '@capacitor/network';
+import { Geolocation } from '@capacitor/geolocation';
+
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +12,34 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  public position:Array<{latitude: number, longitude: number, date: Date}> = [];
+  myDate = new Date();
+
+  ngOnInit(){
+    this.displayConnection();
+
+    this.getPosition();
+  }
+
+  async displayConnection(){
+    const status = await Network.getStatus();
+
+    await Toast.show({
+      text: "Connecté en "+ status.connectionType,
+    });
+  }
+
+  async getPosition(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    
+    this.position = [{
+      latitude: coordinates.coords.latitude, 
+      longitude: coordinates.coords.longitude,
+      date: this.myDate
+    }]
+
+    console.log(this.position);
+  }
 
 }
